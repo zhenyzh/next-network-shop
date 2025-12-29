7c8adc52f9091fb1021f2d8ed3ca19da205c0a80
Evgeny Zherdev <zheny.zherdev@bk.ru>
2025-12-29 19:54:24 +0700

    fix: import

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import { Button } from "@zheny-zh/common-ui";
+import {Button} from '@zhenyzh/common-ui';
 
 export default function Header() {
   return (
     <header>
-      <Button variant="primary">Header</Button>
+      <Button variant='primary'>Header</Button>
     </header>
   );
 }
