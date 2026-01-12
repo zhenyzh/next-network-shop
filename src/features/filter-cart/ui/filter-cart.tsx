ceb19751cfc5e1814a448a141503252e5876ff3b
Evgeny Zherdev <zheny.zherdev@bk.ru>
2026-01-12 12:08:53 +0700

    fix

--- src/features/filter-cart/ui/filter-cart.tsx
+++ src/features/filter-cart/ui/filter-cart.tsx
@@ -1,11 +1,12 @@
 import {Card, CheckBox, Container, Typography} from '@zhenyzh/common-ui/components';
+import s from './filter-cart.module.scss';
 
 export function FilterCart() {
   return (
     <Container>
-      <Card>
+      <Card className={s.gapCard}>
         <Typography variant='h2'>Фильтр</Typography>
-        <CheckBox label='первый' checked />
+        <CheckBox labelClassName={s.label} label='Первый' />
       </Card>
     </Container>
   );
